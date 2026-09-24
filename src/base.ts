45a2ea3927d45d4db13b8977280c486cd1ae2d81
Vivien Graffin <graffinvivien@gmail.com>
2026-09-24 12:36:36 +0200

    export DEFAULT_LOCALE_FORMAT

--- src/base.ts
+++ src/base.ts
@@ -15,7 +15,7 @@ export {
 
 export { TranslationBase } from "@/common/translationClass.ts";
 
-export { DEFAULT_FALLBACK_LOCALE, DEFAULT_SEPARATOR } from "@/common/const.ts";
+export { DEFAULT_FALLBACK_LOCALE, DEFAULT_SEPARATOR, DEFAULT_LOCALE_FORMAT } from "@/common/const.ts";
 
 export type * from "@/common/types.ts";
 
